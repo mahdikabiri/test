e197e62ac9be1b3c2e97ee72b8a91ac8cfd64b4b
mahdi <mahdikabiri47@gmail.com>
2020-07-09 15:06:59 +0430

    test comment added

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -10,11 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    //salaaaam
+        //salaaaam
         //test for push
+
     }
-    
-    public void fun1(){}
-    
-        
+
+    public void fun1() {
+        //say goodbye
+    }
+
+
 }
